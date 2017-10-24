185f346addcd17c4ff8a3a84792c4409d250e1da
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-10-24 14:13:29 -0400

    Refined functions

--- faceDetection.py
+++ faceDetection.py
@@ -1,9 +1,11 @@
 import requests
 import json
 
-def detectFace():
-    outFile = open("coordinates.json", "w")
+img_height = 720
+img_width = 720
 
+# make the post request to skybiometry for face detection and return resulting json
+def detectFace():
     api_key = "n3vg0dfc4j42o0ap4b54mm0msg"
     api_secret = "l20v0d9j4a7bnb7lqcqps7rddm"
 
@@ -13,11 +15,18 @@ def detectFace():
     r = requests.get(url)
 
     data = json.loads(r.content)
-    outFile.write(json.dumps(data[u'photos'][0][u'tags'][0]))
-    print("Saved to coordinates.json")
-    outFile.close()
+    return (data[u'photos'][0][u'tags'][0])
 
 
+# returns {width, height, center: {x,y}} of face in pixels
+def getFaceDimensions():
+    details = detectFace()
 
+    width = (details[u'width']/100.0)*img_width
+    height = (details[u'height']/100.0)*img_height
 
+    center_x = (details[u'center'][u'x']/100.0)*img_width
+    center_y = (details[u'center'][u'y']/100.0)*img_height
+
+    return {"width": width, "height": height, "center": {"x": center_x, "y": center_y}}
 
