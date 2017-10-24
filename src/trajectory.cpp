8f0d2d647d26ddfbea39063eabce776763d6c64e
Edwin Zhang <edwinzhang64@gmail.com>
2017-10-24 11:46:03 -0400

    Added trajectory.cpp

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -1,28 +1,47 @@
-#include "trajectory.h"
+#include "./include/json.hpp"
+#include <fstream>
 #include <iostream>
 #include <cmath>
-#include <algorithm>
 #include <vector>
 
-using namespace std;
-
-double calcDistX(vector<int> coordinates[], double focal_length, double averageHeight);
+double calcDistX(std::vector<double> &coordinates, double focal_length, double averageHeight);
 double calcDistY(double top_right, double top_left, double bottom_right, double bottom_left);
 double calculateTime(double dist_x, double dist_y);
-double pixels_to_cm(int pixels);
+std::vector<double> parseJson();
 
 int main() {
+  std::vector<double> coordinates = parseJson();
+  calcDistX(coordinates, 2570, 0.22);
   return 0;
 }
 
-double calcDistX(vector<int> coordinates[], double focal_length, double averageHeight);
-
-double calculateTime(double dist_x, double dist_y) {
+double calcDistX(std::vector<double> &coordinates, double focal_length, double averageWidth) {
+  double dist_x = 0;
+  dist_x = (averageWidth * focal_length) / (coordinates[0] - coordinates[1]); // horizontal distance = knownWidth * focal_length / pixelWidth
+  std::cout << coordinates[0] << std::endl;
   return dist_x;
 }
 
-double pixels_to_cm(int pixels) {
-  double conversion_factor = 0.5;
-  double cm = (double)pixels * conversion_factor;
-  return cm;
+double calcDistY(double top_right, double top_left, double bottom_right, double bottom_left) {
+  double dist_y = 0;
+  // TODO calculate vertical distance relative to cannon
+  return dist_y;
+}
+
+double calculateTime(double dist_x, double dist_y) {
+  int t = 0;
+  // TODO calculate time needed with dist_x
+  return t;
+}
+
+std::vector<double> parseJson() {
+  std::ifstream i("../data/coordinates.json");
+  nlohmann::json camera_data;
+  i >> camera_data;
+  std::cout << camera_data << std::endl;
+
+  std::vector<double> coordinates = {};
+  // TODO parse data for all coordinates
+  std::cout << coordinates[0] << std::endl;
+  return coordinates;
 }
