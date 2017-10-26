f061a1fe3e6122e794b08735fa84c65547f645e1
derrekchow <derrekdc@gmail.com>
2017-10-26 00:07:52 -0400

    enabled pi to capture image from camera and send that image to the server

--- main.py
+++ main.py
@@ -1,76 +1,56 @@
 from PIL import Image
 import requests
 import base64
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
 def camera():
-	# camera = picamera.PiCamera()
-	# camera.resolution = (720, 720)
-	# camera.capture('data.jpg')
-	# img = Image.open('data.jpg')
-	# img.save('data.jpg')
+	camera = picamera.PiCamera()
+	camera.resolution = (720, 720)
+	camera.capture('data.jpg')
+	camera.close()
 
 	with open("data.jpg", "rb") as image_file:
 		encoded_string = base64.b64encode(image_file.read())
 	
 	print("posting image...")
 	r = requests.post("http://18.221.15.32/save.php", data={'content': encoded_string})
-	print(r.status_code)
+	print(r.content)
 
-camera()
-
-# import requests
-# import RPi.GPIO as GPIO
-# from time import sleep
-
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-
-# class Servo():
-# 	def __init__(self, PIN):
-# 		self.PIN = PIN
-# 		GPIO.setup(self.PIN, GPIO.OUT)
-# 		self.pwm = GPIO.PWM(self.PIN, 50)
-# 		self.pwm.start(7.5)
-# 		self.angle = 0
+class Servo():
+	def __init__(self, PIN):
+		self.PIN = PIN
+		GPIO.setup(self.PIN, GPIO.OUT)
+		self.pwm = GPIO.PWM(self.PIN, 50)
+		self.pwm.start(7.5)
+		self.angle = 0
 		
-# 	def setAngle(self, angle):
-# 		self.pwm.ChangeDutyCycle(0.053*angle + 2.2)
+	def setAngle(self, angle):
+		self.pwm.ChangeDutyCycle(0.053*angle + 2.2)
 		
-# 	def stop(self):
-# 		self.pwm.stop()
+	def stop(self):
+		self.pwm.stop()
 
-# 	def adjust(self, y):
-# 		self.adjustAngle(angle + y)
+	def adjust(self, y):
+		self.adjustAngle(angle + y)
 
-# def camera():
-# 	# camera = picamera.PiCamera()
-# 	# camera.resolution = (720, 720)
-# 	# camera.capture('data.jpg')
-# 	# img = Image.open('data.jpg')
-# 	# img.save('data.jpg')
+def loop():
+	while True:
+		newAngle = input("angle (-1 to take picture): ")
+		if(newAngle == -1):
+			camera()
+		else:
+			servoY.setAngle(newAngle)
 
-# 	with open("data.jpg", "rb") as image_file:
-# 		encoded_string = base64.b64encode(image_file.read())
-	
-# 	print("posting image...")
-# 	r = requests.post("http://18.221.15.32/save.php", data={'content': encoded_string})
-# 	print("image posted")
-# 	#camera.close()
-# 	#GPIO.output(LED3, ON)
-# 	#GPIO.output(LED2, OFF)
 
-# def loop():
-# 	while True:
-# 		newAngle = input("angle: ")
-# 		servoY.setAngle(newAngle)
-
-		
-
-# if __name__ == '__main__':		# Program start from here
-# 	try:
-# 		servoY = Servo(18)
-# 		loop()
-# 	except KeyboardInterrupt:	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-# 		servoY.stop()
-# 		GPIO.cleanup()
+if __name__ == '__main__':		# Program start from here
+	try:
+		servoY = Servo(18)
+		loop()
+	except KeyboardInterrupt:	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		servoY.stop()
+		GPIO.cleanup()
 
