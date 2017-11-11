ad053e53ed0f6ff0f7209751340dc25a61f130d1
Edwin Zhang <edwinzhang64@gmail.com>
2017-11-10 22:39:59 -0500

    Added keystrokes

--- src/client.go
+++ src/client.go
@@ -3,26 +3,44 @@ package main
 import (
 	"fmt"
 	"net"
-	keyboard "azul3d.org/engine/keyboard"
+	"github.com/eiannone/keyboard"
 )
 
 func main() {
 	// connect to this socket
 	conn, _ := net.Dial("tcp", "EDWIN-XPS15:12345")
 	buf2 := make([]byte, 1024)
+	err := keyboard.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	msg := "none"
+
+	defer keyboard.Close()
 	defer conn.Close()
 
+	fmt.Println("Press ESC to quit")
 	for {
-		msg := "none"
-		watcher := keyboard.NewWatcher()
-		// Query for the map containing information about all keys
-		status := watcher.States()
-		left := status[keyboard.ArrowLeft]
-		if left == keyboard.Down {
+		char, key, err := keyboard.GetKey()
+		if (err != nil) {
+			panic(err)
+		} else if (key == keyboard.KeyEsc) {
+			break
+		} else if (key == keyboard.KeyArrowDown) {
+			msg = "down"
+		} else if (key == keyboard.KeyArrowUp) {
+			msg = "up"
+		} else if (key == keyboard.KeyArrowLeft) {
 			msg = "left"
+		} else if (key == keyboard.KeyArrowRight) {
+			msg = "right"
 		}
+		fmt.Printf("You pressed: %q\r\n", char)
+
 		buf := []byte(msg)
-		_, err := conn.Write(buf) // Write a message to the server
+		_, err = conn.Write(buf) // Write a message to the server
+		msg = "none"
 		// listen for reply
 		//allocating memory for each integer
 		n, err := conn.Read(buf2) // Read a message from the server
