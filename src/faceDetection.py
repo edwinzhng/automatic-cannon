777327b428bddaa1b4afe574a00788e820acd190
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-11-11 21:53:35 -0500

    trained derrek chow

--- src/faceDetection.py
+++ src/faceDetection.py
@@ -11,17 +11,19 @@ def detectFace():
     api_secret = "l20v0d9j4a7bnb7lqcqps7rddm"
 
     img_url = "http://52.14.199.236/data.jpg"
-    url = "http://api.skybiometry.com/fc/faces/recognize?namespaces=se101&api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&uids=ayush@se101,rollen@se101,derek@se101"
+    url = "http://api.skybiometry.com/fc/faces/recognize?namespaces=se101&api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&uids=derrek@se101,ayush@se101,rollen@se101,derek@se101"
     r = requests.get(url)
     data = json.loads(r.content)
     user = data[u'photos'][0][u'tags'][0][u'uids'][0][u'uid']
     confidence =  data[u'photos'][0][u'tags'][0][u'uids'][0][u'confidence']
     if user == u'rollen@se101':
-        name = "Rollen"
+        name = "Rollen D'Souza"
     elif user == u'derek@se101':
-        name = "Derek"
+        name = "Derek Rayside"
     elif user == u'ayush@se101':
-        name = "Ayush"
+        name = "Ayush Kapur"
+    elif user == u'derrek@se101':
+        name = "Derrek Chow"
     os.system("say Target Identified, with " + str(confidence) + " percent accuracy. Hello, "  + name)
     return (data[u'photos'][0][u'tags'][0])
 
--- src/train/train.py
+++ src/train/train.py
@@ -4,12 +4,12 @@ import json
 def detectFace():
     api_key = "n3vg0dfc4j42o0ap4b54mm0msg"
     api_secret = "l20v0d9j4a7bnb7lqcqps7rddm"
-    img_url = "http://www.ibrahimirfan.com/rollen5.jpg"
+    img_url = "http://www.ibrahimirfan.com/d6.jpg"
 
 #    url = "http://api.skybiometry.com/fc/faces/detect?api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&namespaces=se101"
- #   url = "http://api.skybiometry.com/fc/tags/save?api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&tids=TEMP_F@0769c1ef3e529c643b07c4a6011100d7_d9e85c0129c4a_68.25_35.83_0_1,TEMP_F@04a457bc7a3dec2621822ccd00ae011c_d252e8069c129_79.09_86.06_0_1" + "&uid=derek@se101"
-    #url = "http://api.skybiometry.com/fc/faces/status?api_key=" + api_key + "&api_secret=" + api_secret + "&uids=rollen@se101"
-    url = "http://api.skybiometry.com/fc/faces/recognize?api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&uids=rollen@se101"
+ #   url = "http://api.skybiometry.com/fc/tags/save?api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&tids=TEMP_F@0b7f8a323da412635ec90b1401fb016b_dec3106e90d1f_50.70_48.34_0_1,TEMP_F@010799f55da2de8817a8ede801bc01a6_d353379e29a66_44.40_56.19_0_1,TEMP_F@0c3bd5753451c0564486c0c101fb01a4_a28e724f90e35_50.70_55.93_0_1,TEMP_F@0cac4f004c98068205b1e27a02a601f9_cb1ab31b0eb53_67.80_67.24_0_1" + "&uid=derrek@se101"
+    #url = "http://api.skybiometry.com/fc/faces/train?api_key=" + api_key + "&api_secret=" + api_secret + "&uids=derrek@se101"
+    url = "http://api.skybiometry.com/fc/faces/recognize?api_key=" + api_key + "&api_secret=" + api_secret + "&urls=" + img_url + "&uids="
 
     r = requests.get(url)
     data = json.loads(r.content)
