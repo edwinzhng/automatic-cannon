f704eb77447423d32a1e2d10bf2f5419872661c0
Edwin Zhang <edwinzhang64@gmail.com>
2017-11-18 22:54:03 -0500

    fixed server

--- src/server.py
+++ src/server.py
@@ -1,13 +1,39 @@
 import socket
 
-s = socket.socket()
-host = "192.168.43.104" #ip of raspberry pi
-port = 12345
-s.bind((host, port))
+def startServer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "192.169.43.104"
+    port = 12345
+    s.bind((host, port))
 
-s.listen(5)
-while True:
-  c, addr = s.accept()
-  print ('Got connection from', addr)
-  c.send('Thank you for connecting')
-  c.close()
+    print
+    s.listen(5)
+    c, addr = s.accept()
+    print("Got connection from", addr)
+
+    while True:
+        data = c.recv(port)
+        if data == "down":
+            c.send("Moving servoY " + data)
+            print(data)
+            #servoY.setAngle(ServoY.angle - 5)
+        elif data == "up":
+            c.send("Moving servoY " + data)
+            print(data)
+            #servoY.setAngle(ServoY.angle + 5)
+        elif data == "left":
+            c.send("Moving servoX " + data)
+            print(data)
+            # servoX.setAngle(ServoX.angle - 5)
+        elif data == "right":
+            c.send("Moving servoX " + data)
+            print(data)
+            #servoX.setAngle(ServoX.angle + 5)
+        elif data == "fire":
+            c.send("Firing now!")
+            #servoT.unlock()
+        elif data == "esc":
+            c.send("Closing server, goodbye.")
+            print("Exiting manual control")
+            break;
+    s.close()
